86110734671aa2b7e6ef8b7b2bf3e426be131ba8
Andrey.Azarov <azarov.andrey1995@yandex.ru>
2021-09-27 16:44:41 +0300

    Shop

---
+++ backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    description:
+      "PlayStation 5 (PS5) - это домашняя игровая консоль, разработанная Sony Interactive Entertainment. Анонсированная в 2019 году как преемница PlayStation 4, PS5 была выпущена 12 ноября 2020 года в Австралии, Японии, Новой Зеландии, Северной Америке, Сингапуре и Южной Корее, а затем 19 ноября 2020 года на других основных рынках, кроме Китая и других стран. Индия.",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "Iphone 12",
+    imageUrl:
+      "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
+    description:
+      "Добро пожаловать в новую эру iPhone. Прекрасно яркий 6,1-дюймовый дисплей Super Retina XDR.1 Керамический экран с улучшенной в 4 раза характеристикой падения.2 Невероятная фотография при слабом освещении с ночным режимом на всех камерах. Запись, редактирование и воспроизведение видео в формате Dolby Vision кинематографического уровня. Мощный чип A14 Bionic. А новые аксессуары MagSafe для легкого подключения и более быстрой беспроводной зарядки.3 Начните веселье.",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "Cannon EOS-1D",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "EOS-1D X сочетает в себе скорость и качество изображения, чтобы создать камеру следующего поколения для профессионалов. Полнокадровый 18-мегапиксельный сенсор с двумя процессорами «DIGIC 5+» устанавливает стандарт, а съемка со скоростью до 12 кадров в секунду выходит за его пределы.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
+    description:
+      "Он поддерживает голосовое взаимодействие, воспроизведение музыки, создание списков дел, установку сигналов тревоги, потоковую передачу подкастов, воспроизведение аудиокниг и предоставление информации о погоде, дорожном движении, спорте и другой информации в реальном времени, например новостей. Alexa также может управлять несколькими интеллектуальными устройствами, используя себя в качестве системы домашней автоматизации.",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "Наушники Audio Technica",
+    imageUrl:
+      "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Оснащенные 45-миллиметровыми динамическими драйверами с большой апертурой и закрытой задней частью, наушники ATH-M50x обеспечивают четкость, глубокие басы и расширенную полосу пропускания (от 15 Гц до 28 кГц), изолируя вас от посторонних звуков.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "JBL FLIP 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+    description:
+      "JBL Flip 4 - это следующее поколение отмеченной наградами серии Flip; это портативный динамик Bluetooth, обеспечивающий удивительно мощный стереозвук. Этот компактный динамик питается от литий-ионного аккумулятора емкостью 3000 мАч, который обеспечивает до 12 часов непрерывного воспроизведения высококачественного звука.",
+    price: 140,
+    countInStock: 10,
+  },
+];
+
+module.exports = products;
---
+++ frontend/src/screens/Cart/CartScreen.js
@@ -0,0 +1,74 @@
+import "./CartScreen.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+// Components
+import CartItem from "../../components/CartItem/CartItem";
+
+// Actions
+import { addToCart, removeFromCart } from "../../redux/actions/cartActions";
+
+const CartScreen = () => {
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {}, []);
+
+  const qtyChangeHandler = (id, qty) => {
+    dispatch(addToCart(id, qty));
+  };
+
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const getCartSubTotal = () => {
+    return cartItems
+      .reduce((price, item) => price + item.price * item.qty, 0)
+      .toFixed(2);
+  };
+
+  return (
+    <>
+      <div className="cartscreen">
+        <div className="cartscreen__left">
+          <h2>Корзина покупок</h2>
+
+          {cartItems.length === 0 ? (
+            <div>
+              Нет покупок <Link to="/">В магазин</Link>
+            </div>
+          ) : (
+            cartItems.map((item) => (
+              <CartItem
+                key={item.product}
+                item={item}
+                qtyChangeHandler={qtyChangeHandler}
+                removeHandler={removeFromCartHandler}
+              />
+            ))
+          )}
+        </div>
+
+        <div className="cartscreen__right">
+          <div className="cartscreen__info">
+            <p>Промежуточный итог({getCartCount()}) покупок</p>
+            <p>${getCartSubTotal()}</p>
+          </div>
+          <div>
+            <button>Оформить заказ</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartScreen;
